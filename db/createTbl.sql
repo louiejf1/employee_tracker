fef4cc551fbaffcaeb1d0fd0a5815ee2e9ecbcda
Louie <louiejf1@gmail.com>
2020-05-02 15:03:44 -0700

    5-2-2020 @ 303PM

---
+++ db/createTbl.sql
@@ -0,0 +1,24 @@
+USE employee_tracker;
+
+CREATE TABLE employee (
+  id int NOT NULL AUTO_INCREMENT,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id int,
+  manager_id int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_role (
+  id int NOT NULL AUTO_INCREMENT,
+  title varchar(30) NOT NULL,
+  salary decimal(10,2) NOT NULL,
+  department_id int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_department (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO employee (first_name) VALUES ('Meeses');
+INSERT INTO employee (last_name) VALUES ('Bobbi');
+INSERT INTO employee (role_id) VALUES (10);
+INSERT INTO employee (manager_id) VALUES (1);
+
+
+INSERT INTO employee_role (title) VALUES ('Tech');
+INSERT INTO employee_role (salary) VALUES (5.00);
+INSERT INTO employee_role (department_id) VALUES (1);
+
+INSERT INTO employee_department (name) VALUES ('Systems');
+
+
+
